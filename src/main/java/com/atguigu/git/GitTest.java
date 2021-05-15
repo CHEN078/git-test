1a755273de002c05f55197b5c23f1bc677db4f3f
Chen <CHENJZ078@163.com>
2021-05-15 15:34:30 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author CHEN
+ * @create 2021-05-15 15:27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
